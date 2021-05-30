bcb47211bcef1c3c0f22ced616a1d2bf749dd451
Yasin <Yasinbeyad@gmail.com>
2021-05-30 18:17:53 +0430

    Work on Internet Timer

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine.UI;
+using CodeStage.AntiCheat.Storage;
 
 public class SpinWheel : MonoBehaviour
 {
@@ -10,31 +11,70 @@ public class SpinWheel : MonoBehaviour
 	public List<AnimationCurve> animationCurves;
 	public bool TimerOn;
 	public float Timer;
+	public GameObject TimeManager = null;
 	private bool spinning;	
 	private float anglePerItem;	
 	private int randomTime;
 	private int itemNumber;
 	public Text timeText;
-	float timeToDisplay = 120;
-
+	float timeToDisplay = 86400f;
+	float Secound;
+	private TimeSpan timeSpan;
+	
 	void DisplayTime()
-	{
-		timeToDisplay -= Time.deltaTime;
-		float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-		float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-		float Hour = Mathf.FloorToInt(timeToDisplay / 60 /60);
+    {
 
-		timeText.text = string.Format("{0:00}:{1:00}:{2:00}",Hour, minutes, seconds);
+
+	
+		TimeSpan t = TimeSpan.FromSeconds(timeToDisplay);
+		//t = t.Subtract(ts);
+		timeToDisplay -= Time.deltaTime * 1.32f;
+
+		//float minutes = Mathf.FloorToInt(timeToDisplay / 60 %60);
+		//float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+		//float Hour = Mathf.FloorToInt(timeToDisplay / 60 /60);
+		print(t);
+		//timeText.text = string.Format("{0:00}:{1:00}:{2:00}",Hour, minutes, seconds);
+		timeText.text = t.Hours + ":" + t.Minutes + ":" + t.Seconds ;
+		 
+	}
+
+	private void OnApplicationQuit()
+	{
+		DateTime QuitTime = DateTime.Now;
+		ObscuredPrefs.SetString("_QuitTime", QuitTime.ToString());
 	}
 	void Start()
 	{
+		string LoadTime = ObscuredPrefs.GetString("_QuitTime");
+		if (!LoadTime.Equals(""))
+		{
+			DateTime LoadDateTime = DateTime.Parse(LoadTime);
+			DateTime NowTime = DateTime.Now;
+			if (NowTime > LoadDateTime)
+			{
+				TimeSpan timeSpan = NowTime - LoadDateTime;
+				Secound = (float)timeSpan.TotalSeconds;
+				//timeToDisplay -= Secound;
+				//Debug.Log(Secound);
+				DisplayTime();
+			}
+
+		}
+		StartCoroutine("plusTime");
+
 		TimerOn = false;
 		spinning = false;
 		anglePerItem = 360 / _prize.Count;
 
 	}
+	IEnumerator plusTime()
+	{
+		yield return new WaitForSeconds(1);
+		timeToDisplay -= 1;
+	}
 
-		void Update()
+	void Update()
 		{
 			
 			if (Input.GetKeyDown(KeyCode.Space) && !spinning && !TimerOn)
